c445a5ed5a9f34852aa54728dc717f95f6118943
gjl97014 <gk2814@163.com>
2020-10-26 15:22:59 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app"><h1>黑马头条新闻发布系统</h1>
+    <!-- 路由出口 -->
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="less"></style>
